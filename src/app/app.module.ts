5ba8da1b5ee1829dccb8d14339e42a42079374b6
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-01-24 16:43:20 +0800

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component'; 
 
 @NgModule({
   declarations: [
@@ -9,7 +8,6 @@ import { PostsComponent } from './posts/posts.component';
   imports: [
     BrowserModule,
     AppComponent,
-    PostsComponent 
   ],
   providers: [],
   bootstrap: []
